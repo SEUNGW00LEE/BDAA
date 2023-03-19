8de4a390e7c578f89ec1d976aa37939f4b67aa1e
seungwoo <seungwoolee99@gmail.com>
2023-03-19 17:31:55 +0900

    0312 수업코드 업로드

--- prac.py
+++ prac.py
@@ -1,18 +1,3 @@
-# 260000
-# 4
-# 20000 5
-# 30000 2
-# 10000 6
-# 5000 8
-
-total = int(input())
-t = int(input())
-sum = 0
-for _ in range(t):
-  price, quantity = map(int, input().split())
-  sum += price * quantity
-
-if total == sum  :
-  print("Yes")
-else :
-  print("No")
+while 1:
+  a, b = map(int, input().split())
+  print(a+b)
